aba043078d997710697247e2b4bc170e716e593e
ISAIASNIAUPARI <niauparii@gmail.com>
2026-08-30 23:45:56 -0500

    fix: rebuild admin panel con clientId correcto 8a7b8685

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: 'C:/Users/ASUS/AppData/Local/Temp/tina-build-temp/tina/__generated__/.cache/1788149689037', url: 'https://content.tinajs.io/2.4/content/8a7b8685-4514-4b1b-96f0-16741fa83f2e/github/main', token: '58c80d2ef9e567d5d58ada1e9dd869fc6f6a096f', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/ASUS/AppData/Local/Temp/tina-build-temp/tina/__generated__/.cache/1788151470262', url: 'https://content.tinajs.io/2.4/content/8a7b8685-4514-4b1b-96f0-16741fa83f2e/github/main', token: '58c80d2ef9e567d5d58ada1e9dd869fc6f6a096f', queries,  });
 export default client;
   
